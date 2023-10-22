6d631d3c6e48ee87d97609628fb2e6915c27dcab
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-10-22 10:25:26 +0700

    feat(baskets): add application constructor

---
+++ baskets/internal/application/application.go
@@ -0,0 +1,75 @@
+package application
+
+import (
+	"context"
+	"eda-in-golang/baskets/internal/domain"
+	"eda-in-golang/internal/ddd"
+)
+
+type (
+	StartBasket struct {
+		ID         string
+		CustomerID string
+	}
+
+	CancelBasket struct {
+		ID string
+	}
+
+	CheckoutBasket struct {
+		ID        string
+		PaymentID string
+	}
+
+	AddItem struct {
+		ID        string
+		ProductID string
+		Quantity  int
+	}
+
+	RemoveItem struct {
+		ID        string
+		ProductID string
+		Quantity  int
+	}
+
+	GetBasket struct {
+		ID string
+	}
+
+	App interface {
+		StartBasket(ctx context.Context, start StartBasket) error
+		CancelBasket(ctx context.Context, cancel CancelBasket) error
+		CheckoutBasket(ctx context.Context, checkout CheckoutBasket) error
+		AddItem(ctx context.Context, add AddItem) error
+		RemoveItem(ctx context.Context, remove RemoveItem) error
+		GetBasket(ctx context.Context, get GetBasket) (*domain.Basket, error)
+	}
+
+	Application struct {
+		basketRepository  domain.BasketRepository
+		storeRepository   domain.StoreRepository
+		productRepository domain.ProductRepository
+		orderRepository   domain.OrderRepository
+		domainPublisher   ddd.EventPublisher
+	}
+)
+
+// interface checker
+var _ App = (*Application)(nil)
+
+func New(
+	basketRepository domain.BasketRepository,
+	storeRepository domain.StoreRepository,
+	productRepository domain.ProductRepository,
+	orderRepository domain.OrderRepository,
+	domainPublisher ddd.EventPublisher,
+) *Application {
+	return &Application{
+		basketRepository:  basketRepository,
+		storeRepository:   storeRepository,
+		productRepository: productRepository,
+		orderRepository:   orderRepository,
+		domainPublisher:   domainPublisher,
+	}
+}
