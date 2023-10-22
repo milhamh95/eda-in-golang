706b832cd6b16fe4117e61aa254e32cfa1896155
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-10-22 14:40:36 +0700

    feat(mallbots): initialize main app

---
+++ cmd/mallbots/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"database/sql"
+	"eda-in-golang/baskets"
+	"eda-in-golang/internal/config"
+	"eda-in-golang/internal/logger"
+	"eda-in-golang/internal/monolith"
+	"eda-in-golang/internal/rpc"
+	"eda-in-golang/internal/waiter"
+	"eda-in-golang/internal/web"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() (err error) {
+	var cfg config.AppConfig
+	// parse config/env/...
+	cfg, err = config.InitConfig()
+	if err != nil {
+		return err
+	}
+
+	m := app{cfg: cfg}
+
+	// init infrastructure...
+	m.db, err = sql.Open("pgx", cfg.PG.Conn)
+	if err != nil {
+		return err
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			return
+		}
+	}(m.db)
+	m.logger = logger.New(logger.LogConfig{
+		Environment: cfg.Environment,
+		LogLevel:    logger.Level(cfg.LogLevel),
+	})
+	m.rpc = initRpc(cfg.Rpc)
+	m.mux = initMux(cfg.Web)
+	m.waiter = waiter.New(waiter.CatchSignals())
+
+	// init modules
+	m.modules = []monolith.Module{
+		&baskets.Module{},
+	}
+
+	if err = m.startupModules(); err != nil {
+		return err
+	}
+
+	// Mount general web resources
+	m.mux.Mount("/", http.FileServer(http.FS(web.WebUI)))
+
+	fmt.Println("started mallbots application")
+	defer fmt.Println("stopped mallbots application")
+
+	m.waiter.Add(
+		m.waitForWeb,
+		m.waitForRPC,
+	)
+
+	return m.waiter.Wait()
+}
+
+func initRpc(_ rpc.RpcConfig) *grpc.Server {
+	server := grpc.NewServer()
+	reflection.Register(server)
+
+	return server
+}
+
+func initMux(_ web.WebConfig) *chi.Mux {
+	return chi.NewMux()
+}
